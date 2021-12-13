ae528f24ef4308a99db629966153b64c14210e5f
Mordabar <95420982+Mordabar@users.noreply.github.com>
2021-12-13 23:47:24 +0100

    Finalizacion de projecto
    se finaliza el diseño de todas las pestañas

--- js/script.js
+++ js/script.js
@@ -548,9 +548,25 @@ form &&
     });
   });
   
+/* ================================================
+      Slow id movement
+      ================================================ */
+ 
 
-
-
+  $(document).ready(function() {
+    $('a[href^="#"]').click(function() {
+      var destino = $(this.hash);
+      if (destino.length == 0) {
+        destino = $('a[name="' + this.hash.substr(1) + '"]');
+      }
+      if (destino.length == 0) {
+        destino = $('html');
+      }
+      $('html, body').animate({ scrollTop: destino.offset().top }, 1000);
+      return false;
+    });
+  });
+ 
 /* ================================================
     Change Specific Hour and update status
     ================================================ */
